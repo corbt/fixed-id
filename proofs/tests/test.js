1c017b0331060769e9cdeded7edb2f72e0eef859
Kyle Corbitt <kyle@corbt.com>
2021-09-29 12:32:41 +0200

    switched to poseidon proof for performance

--- proofs/tests/test.js
+++ proofs/tests/test.js
@@ -2,22 +2,40 @@ const { MerkleTree } = require("merkletreejs");
 const { initialize } = require("zokrates-js/node");
 const fs = require("fs");
 const path = require("path");
-const crypto = require("crypto");
 
-const hashFn = (input) => crypto.createHash("sha256").update(Buffer.from(input, "hex")).digest();
+const poseidon = require("circomlib/src/poseidon");
 
-const leaves = ["a", "b", "c", "d"].map((x) => hashFn(Buffer.from(x).toString("hex")));
-const tree = new MerkleTree(leaves, hashFn);
+// Conversion/serialization helpers
 
-// Takes a NodeJS buffer and splits it into an array of N strings each
-// representing a 32-byte unsigned int, which is the format Zokrates seems
-// happiest with.
+const hexToBigInt = (hex) => BigInt("0x" + hex);
+const bigIntToHex = (bigInt) => {
+  let str = bigInt.toString(16);
+  if (str.length % 2 !== 0) str = "0" + str;
+  return str;
+};
+const bufferToBigInt = (buffer) => hexToBigInt(buffer.toString("hex"));
+const bigIntToBuffer = (bigInt) => Buffer.from(bigIntToHex(bigInt), "hex");
 const bufferToZok32 = (buffer) =>
   buffer
     .toString("hex")
     .split(/(.{8})/)
     .filter((c) => c.length > 0)
     .map((chunk) => parseInt(chunk, 16).toString());
+const bufferToZokField = (buffer) => "0x" + bufferToBigInt(buffer).toString(16);
+
+// Adapt the poseidon hash function to work with `merkletreejs`.
+const treePoseidon = (input) => {
+  const lhs = input.slice(0, input.length / 2);
+  const rhs = input.slice(input.length / 2, input.length);
+  return bigIntToBuffer(poseidon([bufferToBigInt(lhs), bufferToBigInt(rhs)]));
+};
+
+const hashFn = treePoseidon;
+
+const leaves = [1, 2, 3, 4].map((x) => bigIntToBuffer(poseidon([x])));
+const tree = new MerkleTree(leaves, hashFn);
+
+console.log(tree.toString());
 
 async function main() {
   const zok = await initialize();
@@ -29,14 +47,15 @@ async function main() {
   const merkleProof = tree.getProof(leaf);
 
   const { witness, output } = zok.computeWitness(artifacts, [
-    bufferToZok32(tree.getRoot()),
-    bufferToZok32(leaf),
+    bufferToZokField(tree.getRoot()),
+    bufferToZokField(leaf),
     merkleProof.map((step) => ({
       isRightNode: step.direction === "right",
-      otherDigest: bufferToZok32(step.data),
+      otherDigest: bufferToZokField(step.data),
     })),
   ]);
 
+  console.log(output);
   console.log("Merkle proof valid");
 }
 
